0cfa3f0c2b9d97fe304329303128fff50f1f9063
Renan Oliveira <renan@corp.globo.com>
2015-06-07 17:51:31 -0300

    Removendo comentarios

--- polynomials/karatsuba.py
+++ polynomials/karatsuba.py
@@ -26,14 +26,11 @@ def karatsuba(num1, num2):
 	if (num1 / 10 == 0) or (num2 / 10 == 0):
 		return num1 * num2
 
-	# calculates the size of the numbers
 	m2 = m_power(num1,num2)
 
-	# split the digit sequences about the middle
 	high1, low1 = split_at(num1, m2)
 	high2, low2 = split_at(num2, m2)
 
-	# 3 calls made to numbers approximately half the size
 	z0 = karatsuba(low1,low2)
 	z1 = karatsuba((low1+high1),(low2+high2))
 	z2 = karatsuba(high1,high2)
