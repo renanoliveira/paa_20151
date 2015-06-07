6cb96598bbd8c6e582facb41b710fbc83c1976a6
Renan Oliveira <renan@corp.globo.com>
2015-06-07 17:47:57 -0300

    Removendo comentario

--- polynomials/multi_n2.py
+++ polynomials/multi_n2.py
@@ -7,7 +7,6 @@ import sys
 def multiply_polynomials_in_n2(poly1, poly2):
     polynomials_size = len(poly1)+len(poly2)
     buckets = [0]*(polynomials_size-1)
-    print("Buckets = {0}".format(buckets))
     for index1, value1 in enumerate(poly1): # O(n)
         for index2,value2 in enumerate(poly2): # O(n)
             buckets[index1+index2] += value1*value2
