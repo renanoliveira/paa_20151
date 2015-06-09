954e3ccbe84c838b7db2a4e6788e36f22578f2d8
Renan Oliveira <renan@corp.globo.com>
2015-06-09 12:07:50 -0300

    Lendo o arquivo e printando os resultados. Falta o CSV (fácil)

--- polynomials/multi_n2.py
+++ polynomials/multi_n2.py
@@ -3,45 +3,54 @@
 
 import random
 import sys
+import os
+import time
 
 def multiply_polynomials_in_n2(poly1, poly2):
     polynomials_size = len(poly1)+len(poly2)
     buckets = [0]*(polynomials_size-1)
     for index1, value1 in enumerate(poly1): # O(n)
         for index2,value2 in enumerate(poly2): # O(n)
-            buckets[index1+index2] += value1*value2
-            print("Buckets = {0}".format(buckets))
-
-    print("Product of the polynomials = {0}".format(buckets))
+            buckets[index1+index2] += int(value1)*int(value2)
     return buckets
 
+def generate_polynomials(line):
+    f = open("instancias/" + line, 'r')
+    num_linha = 1
+    grau_do_polinomio = 0
+    polinomio_1 = []
+    polinomio_2 = []
+    segunda_lista = False
+    for linha in f.readlines():
+        if num_linha == 1:
+            grau_do_polinomio = int(linha.strip())
+        else:
+            if len(linha.split()) == 0:
+                segunda_lista = True
 
-def generate_polynomial(size):
-    items = []
-    for i in range(size):
-        random_value = random.uniform(1.0, 100.0)
-        items.append(random_value)
+            if not segunda_lista:
+                polinomio_1.append(linha.split())
+            else:
+                polinomio_2.append(linha.split())
+        num_linha = num_linha + 1
 
-    return items
+
+    poly1 = sum(polinomio_1, [])
+    poly2 = sum(polinomio_2, [])
+
+    f.close()
+    return grau_do_polinomio, poly1, poly2
 
 
 if __name__ == "__main__":
+    files = os.listdir("instancias")
+    for filename in files:
+        grau, poly1, poly2 = generate_polynomials(filename)
 
-    poly1 = []
-    poly2 = []
+        print("===> Grau dos Polinomios: {0}".format(grau))
+        t = time.clock()
 
-    if sys.argv[1] == "test":
-        print "===> Modo teste para validar alogritmo"
-        poly1 = [1,2,3]
-        poly2 = [4,5,6]
+        multiply_polynomials_in_n2(poly1, poly2)
 
-    elif len(sys.argv) == 3:
-        poly1 = generate_polynomial(int(sys.argv[1]))
-        poly2 = generate_polynomial(int(sys.argv[2]))
-
-        print "===> Gerar polinomios com valorizes randomicos - "
-        print "===> Poly1 : {0} Poly2: {1}".format(poly1, poly2)
-    else:
-        print "==> Não encontrei um padrão"
-
-    multiply_polynomials_in_n2(poly1, poly2)
+        elapsed_time = time.clock() - t
+        print("===> Tempo de execução: {0}".format(elapsed_time))
